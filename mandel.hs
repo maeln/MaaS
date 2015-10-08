cf62798ca8ea2492695fdfaad559c3db2464b738
Maël N <maeln@localhost.localdomain>
2015-10-08 19:31:41 +0200

    Changed the way the original point is used. Now, the point given in argument is always the point at the center of the space.

--- mandel.hs
+++ mandel.hs
@@ -25,7 +25,7 @@ toMandelbrotSpace (dx,dy) (maxi,mayi) (x,y) = (rx*dx :+ ry*dy)
         ry = fromIntegral (y - my)
 
 genComplexSpace :: (Enum a, RealFloat a) => a -> a -> a -> a -> a -> [[Complex a]]
-genComplexSpace xpt ypt resx resy zoom = [ [((xpt+x*r) :+ (ypt+y*r)) | x <- [0.0,1.0..(resx-1)]] | y <- [0.0,1.0..(resy-1)]]
+genComplexSpace xpt ypt resx resy zoom = [ [((x*r+(xpt-(resx/2)*r)) :+ (y*r+(ypt-(resy/2)*r))) | x <- [0.0,1.0..(resx-1)]] | y <- [0.0,1.0..(resy-1)]]
   where r = 1/(zoom*(resx/3.0))
   
 linearInterpol :: Num a => a -> a -> a -> a
