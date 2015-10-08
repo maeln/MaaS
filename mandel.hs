6dcded64975bfc850ef86dbc5b8978f35e8fb944
Maël N <maeln@localhost.localdomain>
2015-10-08 19:06:58 +0200

    Change the way the space is generated.

--- mandel.hs
+++ mandel.hs
@@ -4,22 +4,30 @@ import Data.Complex
 import Data.Matrix
 import Data.Word
 
-{-
-c = -0.1528 + 1.0397i
-x = [-2;1]
-y = [-1;1]
-mapping matrix (x,y):
-tx = -2 + 3/x * dx
-ty = 1 -2/y * dy
-
-150 200
--}
+import Control.Parallel
+import Control.Parallel.Strategies
 
 -- Space / Mathy stuff 
 switchSpace :: (RealFrac a, Integral b) => (b,b) -> (b,b) -> Complex a 
 switchSpace (x,y) (dx,dy) = ( (-2 + 3/fi(x) * fi(dx)) :+ (1 - 2/fi(y) * fi(dy)) ) 
                             where fi = fromIntegral
+{- A PROPOS :
+Pour le moment, switchSpace fonction en donnant les bornes de l'espace,
+à l'avenir, ce qu'il faudrait faire c'est donnée un point dans le plan complexe
+un delta X,Y pour chaque pixel, et determiner l'espace couvert avec la résolution donnée.
+-}
 
+toMandelbrotSpace :: (RealFrac a, Integral b) => (a,a) -> (b,b) -> (b,b) -> Complex a
+toMandelbrotSpace (dx,dy) (maxi,mayi) (x,y) = (rx*dx :+ ry*dy)
+  where mx = maxi `div` 2
+        my = mayi `div` 2
+        rx = fromIntegral (x - mx)
+        ry = fromIntegral (y - my)
+
+genComplexSpace :: (Enum a, RealFloat a) => a -> a -> a -> a -> a -> [[Complex a]]
+genComplexSpace xpt ypt resx resy zoom = [ [((xpt+x*r) :+ (ypt+y*r)) | x <- [0.0,1.0..(resx-1)]] | y <- [0.0,1.0..(resy-1)]]
+  where r = 1/(zoom*(resx/3.0))
+  
 linearInterpol :: Num a => a -> a -> a -> a
 linearInterpol x y r = x * (1-r) + y * r
 
@@ -50,39 +58,31 @@ genColor cols m n
   where dm = fromIntegral m
         dn = fromIntegral n
 
-genImage :: Integral a => Matrix a -> a -> [(Word8,Word8,Word8)]
-genImage matrix m = map (convColor . genColor [Color 0.2 0.3 0.4, Color 0.85 0.35 0.31, Color 0.3 0.5 0.8] m) (toList (transpose matrix))
-
 convColor :: RealFrac a => Color a -> (Word8,Word8,Word8)
 convColor (Color r g b) = (w8 (r*255.0),w8 (g*255.0),w8 (b*255.0))
   where w8 = floor
 
 -- Mandelbrot
 
-{-
-mandelbrot :: (RealFloat a, Integral b) => Complex a -> Complex a -> b -> b
-mandelbrot origin ci mx = aux origin ci mx
-            where aux _ _ 0  = 0
-                  aux z c n | magnitude z <= 2 = aux (z*z+c) c (n-1)
-                  aux z _ n | magnitude z > 2 = n
--}
-
-mandelbrot :: (RealFloat a, Integral b) => Complex a -> Complex a -> b ->b -> b
+mandelbrot :: (RealFloat a, Integral b) => Complex a -> Complex a -> b -> b -> b
 mandelbrot _ _ 0 _ = 0
 mandelbrot z c mx n | (realPart z)**2 + (imagPart z)**2 <= 4 = mandelbrot (z*z+c) c (mx-1) (n+1)
                     | otherwise = n
 
-mandelMapper :: Integral a => a -> (Int,Int) -> (Int,Int) -> a
-mandelMapper mx size pos = mandelbrot (0 :+ 0) (switchSpace size pos) mx 0
+genImage :: Integral b => [b] -> b -> [(Word8,Word8,Word8)]
+genImage space m = map (convColor . genColor [Color 0.85 0.35 0.31, Color 0.3 0.5 0.8] m) space
 
-mandelMatrix :: Integral a => a -> Int -> Int -> Matrix a
-mandelMatrix mx x y = matrix x y (mandelMapper mx (x,y))
+mandel_map :: (NFData a, Enum b, Integral a, RealFloat b) => a -> b -> b -> b -> b -> b -> [a]
+mandel_map m ptx pty x y z = (parMap rdeepseq) (\ n -> mandelbrot (0:+0) n m 0) (concat (genComplexSpace ptx pty x y z))
 
 main = do
-  [f,m,x,y] <- getArgs
+  [f,m,ptx,pty,x,y,z] <- getArgs
   let fm = read m :: Int in
-    let fx = read x :: Int in
-    let fy = read y :: Int in
-    writePPM f (fromIntegral fx, fromIntegral fy) (genImage (mandelMatrix fm fx fy) fm)
+    let px = read ptx :: Double in
+    let py = read pty :: Double in
+    let fx = read x :: Integer in
+    let fy = read y :: Integer in
+    let fz = read z :: Double in
+    writePPM f (fx, fy) (genImage (mandel_map fm px py (fromIntegral fx) (fromIntegral fy) fz) fm)
 
 
